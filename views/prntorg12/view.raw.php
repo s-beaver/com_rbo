b3084fb1e478a2c72f7d45a9219fef849e2a9de3
Sergei Efremov <efremov@siams.com>
2015-10-16 18:54:46 +0500

    Удалили из конфигурации абсолютный путь к компоненте

--- views/prntorg12/view.raw.php
+++ views/prntorg12/view.raw.php
@@ -4,7 +4,7 @@ defined('_JEXEC') or die;
 jimport('joomla.application.component.view');
 define ('RBO_PATH', realpath (dirname (__FILE__) . '/../..'));
 require_once RBO_PATH . "/configuration.php";
-define ('JRBO_PATH', str_replace (RbConfig::$pathRemoveForJsScrips, '', RBO_PATH));
+define ('JRBO_PATH', str_replace (realpath(dirname(__FILE__) . "/../../../.."), '', RBO_PATH));
 
 class RbOViewPrnTorg12 extends JViewLegacy
 {
