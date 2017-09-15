5679ef99704a01906a555240b5d981167b8ee10e
Sergei Efremov <efremov@siams.com>
2017-09-16 00:52:08 +0500

    Убрана возможность выбирать товар в операции. Исправлена ошибка в поле "дата оплаты"

--- views/opers/tmpl/default.php
+++ views/opers/tmpl/default.php
@@ -31,7 +31,7 @@
               </div>
               <div style="float: right">
                 <label for="rbo_opers.pay_date">Оплата</label>
-                <input type="text" name="rbo_opers.pay_date" id="s_bill.pay_date" style='text-align: center; width: 100px' />
+                <input type="text" name="rbo_opers.pay_date" id="rbo_opers.pay_date" style='text-align: center; width: 100px' />
                 <label for="rbo_opers.oper_type">Операция</label>
                 <select id="rbo_opers.oper_type" name="rbo_opers.oper_type" style="width: 200px" autofocus>
                 </select>
@@ -57,8 +57,8 @@
             <td>
               <table>
                 <tr>
-                  <td><label for="prod_search">Поиск</label> <input type="text" name="prod_search" id="prod_search" style='width: 350px' /> 
-                    <a href="" id="prod_search_btn"> <img src="<?php echo JRBO_PATH?>/images/icon-32-search-on.png" /></a>
+                  <td style='visibility:hidden;'><label for="prod_search">Поиск</label> <input type="text" name="prod_search" id="prod_search" style='width:350px' />
+                      <a href="" id="prod_search_btn"> <img src="<?php echo JRBO_PATH?>/images/icon-32-search-on.png" /></a>
                     <a href="" id="prod_search_off_btn"> <img src="<?php echo JRBO_PATH?>/images/icon-32-delete-on.png" /></a>
                   </td>
                 </tr>
@@ -71,13 +71,13 @@
                 <tr>
                   <td>
                     <label for="rbo_opers.product_price">Цена</label>
-                    <input type="text" name="rbo_opers.product_price" id="rbo_opers.product_price" style="text-align: center; width: 50px" /> 
+                    <input type="text" name="rbo_opers.product_price" id="rbo_opers.product_price" style="text-align: center; width: 50px" />
                     <label for="rbo_opers.product_cnt">р.&nbsp;Х&nbsp;</label>
-                    <input type="text" name="rbo_opers.product_cnt" id="rbo_opers.product_cnt" style="text-align: center; width: 30px" /> 
+                    <input type="text" name="rbo_opers.product_cnt" id="rbo_opers.product_cnt" style="text-align: center; width: 30px" />
                     <label for="rbo_opers.oper_sum">ед.&nbsp;=&nbsp;</label>
                     <input type="text" name="rbo_opers.oper_sum" id="rbo_opers.oper_sum" style="text-align: center; width: 50px" />&nbsp;р.
                     <label id="prod_price1">Цена Опт.1</label>
-                    <input style="visibility: hidden; width: 5px" id="rbo_opers.productId" /> 
+                    <input style="visibility: hidden; width: 5px" id="rbo_opers.productId" />
                     <input style="visibility: hidden; width: 5px" id="rbo_opers.product_code" />
                   </td>
                 </tr>
